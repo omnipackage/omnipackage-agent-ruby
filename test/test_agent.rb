3976234db114083875ae9f38fa7d4fb4ae7deeb0
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-01-31 22:40:49 +0100

    ninth+ commit

--- test/test_agent.rb
+++ test/test_agent.rb
@@ -19,26 +19,22 @@ class TestAgent < ::Minitest::Test
 
       distro = ::Agent::Distro.new(res.build_config.fetch(:distro))
       package_artefact = res.artefacts[0]
-      mounts = {
-        package_artefact.dirname => '/pack'
-      }
-      mount_cli = mounts.map do |from, to|
-        "--mount type=bind,source=#{from},target=#{to}"
-      end.join(' ')
+      mounts = { package_artefact.dirname => '/pack' }
+      mount_cli = mounts.map { |from, to| "--mount type=bind,source=#{from},target=#{to}" }.join(' ')
       commands = []
       if distro.rpm?
         commands << "rpm -i /pack/#{package_artefact.basename}"
       elsif distro.deb?
         commands << "dpkg -i /pack/#{package_artefact.basename}"
+      else
+        raise "unsupported distro #{distro}"
       end
       commands << 'sample_project'
       cli = <<~CLI
-        #{::Agent.runtime} run --entrypoint /bin/sh #{mount_cli} #{distro.image} -c "#{commands.join(' && ')}"
+        #{::Agent.runtime} run --rm --entrypoint /bin/sh #{mount_cli} #{distro.image} -c "#{commands.join(' && ')}"
       CLI
       lines = []
-      success = ::Agent::Utils::Subprocess.new.execute(cli) do |output_line|
-        lines << output_line
-      end&.success?
+      success = ::Agent::Utils::Subprocess.new.execute(cli) { |output_line| lines << output_line }&.success?
 
       assert success
       assert_equal 'alive 1.3.5', lines[-1]
