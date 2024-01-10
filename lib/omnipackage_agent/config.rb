95a574cf8ead6b566f70e8115b3380e1a2340597
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2024-01-10 09:56:27 +0100

    refactor config - green rubocop

--- lib/omnipackage_agent/config.rb
+++ lib/omnipackage_agent/config.rb
@@ -27,12 +27,13 @@ module OmnipackageAgent
       lockfiles_dir:      { type: ::String, default: "#{::Dir.tmpdir}/omnipackage-lock" }
     }.freeze
 
-    def initialize(hash, attributes = ATTRIBUTES) # rubocop: disable Metrics/MethodLength, Metrics/AbcSize
+    def initialize(hash, attributes = ATTRIBUTES) # rubocop: disable Metrics/MethodLength, Metrics/AbcSize, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
       attributes.each do |a, meta|
         value = hash.is_a?(::Hash) ? hash[a] : nil
         if meta.key?(:type)
           if value.nil?
             raise ::TypeError, "attribute #{a} does not have default value and must exist" unless meta[:default]
+
             value = if meta[:default].is_a?(::Proc)
                       meta[:default].call
                     else
