eb18ad3d86cef226737936cd2f3f9e0c38b3db57
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2024-12-28 20:56:52 +0100

    add omnipackage build itself

---
+++ .omnipackage/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -xEeuo pipefail
+
+BUILDROOT=$1
+PREFIX="/usr"
+LIBDIR="$PREFIX/libexec/omnipackage-agent-ruby"
+BINDIR="$PREFIX/bin"
+
+install -d -m755 $BUILDROOT/$LIBDIR
+install -d -m755 $BUILDROOT/$BINDIR
+cp -R $(ls -I ".omnipackage" -I ".gitignore" -I ".ruby-version" -I "node_modules" -I "debian") $BUILDROOT/$LIBDIR
+
+ln -s $LIBDIR/exe/omnipackage $BUILDROOT$BINDIR
+ln -s $LIBDIR/exe/omnipackage-agent $BUILDROOT$BINDIR
+
+
+echo "******"
+ls -latrh $BUILDROOT
+echo "******"
